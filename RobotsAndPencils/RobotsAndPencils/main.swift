282896bae18fa57e8ae9380498e9ee9a786b5629
Wellington Moreno <jwellington.moreno@gmail.com>
2017-04-06 09:32:06 -0700

    Adds console application

---
+++ RobotsAndPencils/RobotsAndPencils/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RobotsAndPencils
+//
+//  Created by Wellington Moreno on 4/6/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
